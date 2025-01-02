af4573530057465cd70947aed5a79082acbc6b2c
Manish Tewatia <ManishTewatia3020@gmail.com>
2025-01-02 15:39:34 +0530

    Your commit message

--- script.js
+++ script.js
@@ -1,54 +1,53 @@
-let formBtn = document.querySelector('#login-btn');
-let loginForm = document.querySelector('.login-form-container');
-let formClose = document.querySelector('#form-close');
-let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
-let imgBtn = document.querySelectorAll('.img-btn');
+//let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector(".login-form-container");
+let formClose = document.querySelector("#form-close");
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar");
+let imgBtn = document.querySelectorAll(".img-btn");
 
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-    loginForm.classList.remove('active');
-}
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+  loginForm.classList.remove("active");
+};
 
-menu.addEventListener('click', () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
+menu.addEventListener("click", () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
 });
 
-formBtn.addEventListener('click', () =>{
-    loginForm.classList.add('active');
+formBtn.addEventListener("click", () => {
+  loginForm.classList.add("active");
 });
 
-formClose.addEventListener('click', () =>{
-    loginForm.classList.remove('active');
+formClose.addEventListener("click", () => {
+  loginForm.classList.remove("active");
 });
-imgBtn.forEach(btn =>{
-    btn.addEventListener('click', ()=>{
-        document.querySelector('.controls .active').classList.remove('active');
-        btn.classList.add('active');
-        let src = btn.getAttribute('data-src');
-        document.querySelector('#img-slider').src = src;
-    });
+imgBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(".controls .active").classList.remove("active");
+    btn.classList.add("active");
+    let src = btn.getAttribute("data-src");
+    document.querySelector("#img-slider").src = src;
+  });
 });
 
 var swiper = new Swiper(".review-slider", {
-    spaceBetween: 20,
-    loop:true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
     },
-    breakpoints: {
-        640: {
-          slidesPerView: 1,
-        },
-        768: {
-          slidesPerView: 2,
-        },
-        1024: {
-          slidesPerView: 3,
-        },
+    768: {
+      slidesPerView: 2,
     },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
 });
-
